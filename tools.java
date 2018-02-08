68e8f7dfd178f46482fe8bb067a7f5d99c2fec28
dylanP3495 <palafoxdylan@gmail.com>
2018-02-07 23:25:46 -0800

    ejryntsd

--- tools.java
+++ tools.java
@@ -3,9 +3,11 @@
  * Write a description of class tools here.
  *
  * @author Dylan
- * @version 2.7.18
+ * @version 2.8.18
  */
 public abstract class tools extends Item
 {
-
+    public tools(String getName, ArrayList getStats){
+        
+    }
 }
